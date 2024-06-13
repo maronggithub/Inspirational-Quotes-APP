f3cb55ed12425e6980399f68a2a8974a2ce16ddc
MA RONG <marongreceive@gmail.com>
2024-06-13 11:48:04 +0900

    data request

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Quote() {
+  const [quote, setQuote] = useState('');
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try{
+        const response = await fetch('https://zenquotes.io/api/random');
+        if (!response.ok) {
+          setQuote("The servers are too tired to shut down today.");
+        }
+        const data = await response.json();
+        if(data.length > 0) {
+           setQuote(data[0].h);
+        }
+     } catch (error) {
+          setQuote("The server is too tired to work today");
+     }
+    };
+    fetchQuote();
+  },[]);
+
   return (
     <div id='quote' className='quote'>
-      <p>Inspire yourself!</p>
+      <p>{quote || 'Trying to figure out how to encourage you.....'}</p>
     </div>
   );
 }
