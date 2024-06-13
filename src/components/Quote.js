c86d80f9c4e4b2b32d9308b85de1e7019905f207
MA RONG <marongreceive@gmail.com>
2024-06-13 16:08:52 +0900

    add new ui

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRetweet } from '@fortawesome/free-solid-svg-icons';
 
 function Quote() {
   const [quote, setQuote] = useState('');
@@ -6,13 +8,15 @@ function Quote() {
   useEffect(() => {
     const fetchQuote = async () => {
       try{
-        const response = await fetch('https://zenquotes.io/api/random');
+        const response = await fetch('https://cors-anywhere.herokuapp.com/https://zenquotes.io/api/random');
         if (!response.ok) {
           setQuote("The servers are too tired to shut down today.");
+          return;
         }
         const data = await response.json();
+        console.log(data);
         if(data.length > 0) {
-          setQuote(data[0].h);
+          setQuote(data[0].q);
         }
     } catch (error) {
           setQuote("The server is too tired to work today");
@@ -24,6 +28,7 @@ function Quote() {
   return (
     <div id='quote' className='quote'>
       <p id="quote-text" className='quote-text'>{quote || 'Trying to figure out how to encourage you.....'}</p>
+      <FontAwesomeIcon icon={faRetweet} className='refresh' />
     </div>
   );
 }
